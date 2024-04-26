e64c27ce433db065a814bc94dfe24b182faa73b0
Kunal Raj <kashyapkunalraj1@gmail.com>
2024-04-26 23:34:33 +0530

    Update index.ts

--- utils/index.ts
+++ utils/index.ts
@@ -60,17 +60,18 @@ export async function fetchCars() {
 
 
 
-export const generateCarImageUrl = (car: CarProps, angle?: string) => {
-  const url = new URL("https://cdn.imagin.studio/getimage");
-  const { make, model, year } = car;
-
-  url.searchParams.append('customer', process.env.NEXT_PUBLIC_IMAGIN_API_KEY || '');
-  url.searchParams.append('make', make);
-  url.searchParams.append('modelFamily', model.split(" ")[0]);
-  url.searchParams.append('zoomType', 'fullscreen');
-  url.searchParams.append('modelYear', `${year}`);
-  // url.searchParams.append('zoomLevel', zoomLevel);
-  url.searchParams.append('angle', `${angle}`);
-
-  return `${url}`;
-} 
+export const generateCarImageUrl = (car: CarProps, angle?
+: string) => {
+const url = new URL('https://cdn.imagin.studio/
+getimage');
+const { make, year, model } = car;
+url.searchParams.append('customer',
+'hrjavascript-mastery');
+url.searchParams.append('make', make);
+url.searchParams.append('modelFamily', model.split(' ')
+[0]);
+url.searchParams.append('zoomType', 'fullscreen');
+url.searchParams.append('modelYear', `${year}`);
+url.searchParams.append('angle', `${angle}`);
+return ${url}`;
+}
